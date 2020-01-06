961358b27cf9ce7bf8481037a66fff9830afad55
Bhargavi G. Ray <31646439+raybg@users.noreply.github.com>
2020-01-06 17:10:07 +0530

    Add files via upload

--- LB1.py
+++ LB1.py
@@ -1,16 +1,20 @@
+
 import shutil
 import pandas as pd
-
+from pathlib import Path
 
 shutil.copyfile("C:\\Cogencis WorkStation\\TMP\\Specific\\Common\\QUOTELISTS2.TXT", "C:\\Users\\g.bhargavi\\Desktop\\nowtest12.csv")
 
-
-lb = open("C:\\Users\\BG\\Desktop\\nowtest12.csv", "r+")
+lb = open("C:\\Users\\g.bhargavi\\Desktop\\nowtest12.csv", "r+")
 lb = lb.read()
 x = lb.replace("\n", ",")
-y = x.split(",")
+x = x.replace("@","")
+x = x.replace(";",",")
+
+y = x.split(",",10000)
 
 z = pd.Series(y)
 print(z)
 
-z.to_frame(z,columns=)
+
+
