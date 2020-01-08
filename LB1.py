3207e97bf564a8c1626af65a9909f2d7a27f5553
Bhargavi G. Ray <31646439+raybg@users.noreply.github.com>
2020-01-08 16:02:22 +0530

    Add files via upload

--- LB1.py
+++ LB1.py
@@ -1,20 +1,34 @@
-
-import shutil
 import pandas as pd
-from pathlib import Path
+import numpy as np
+import shutil
+from shutil import *
+import csv
 
-shutil.copyfile("C:\\Cogencis WorkStation\\TMP\\Specific\\Common\\QUOTELISTS2.TXT", "C:\\Users\\g.bhargavi\\Desktop\\nowtest12.csv")
+shutil.copyfile("C:\\Cogencis WorkStation\\TMP\\Specific\\Common\\QUOTELISTS2.TXT", "C:\\Users\\g.bhargavi\\Desktop\\owtest12.csv")
 
-lb = open("C:\\Users\\g.bhargavi\\Desktop\\nowtest12.csv", "r+")
+lb = open("C:\\Users\\g.bhargavi\\Desktop\\owtest12.csv", "r+")
 lb = lb.read()
-x = lb.replace("\n", ",")
-x = x.replace("@","")
-x = x.replace(";",",")
+lb1 = lb.replace("|",",")
 
-y = x.split(",",10000)
+f= open("C:\\Users\\g.bhargavi\\Desktop\\owtest13.txt", "w+")
+f.write(lb1)
+c = shutil.copyfile("C:\\Users\\g.bhargavi\\Desktop\\owtest13.txt","C:\\Users\\g.bhargavi\\Desktop\\owtest13.csv")
 
-z = pd.Series(y)
-print(z)
+lb2 = lb1.replace("@",",")
+f = open("C:\\Users\\g.bhargavi\\Desktop\\owtest14.txt", "w+")
+f.write(lb2)
+d = shutil.copyfile("C:\\Users\\g.bhargavi\\Desktop\\owtest14.txt","C:\\Users\\g.bhargavi\\Desktop\\owtest14.csv")
+
+
+
+
+#lb3 = lb2.replace(";",",")
+#f = open("C:\\Users\\g.bhargavi\\Desktop\\owtest15.txt", "w+")
+#f.write(lb3)
+#e = shutil.copyfile("C:\\Users\\g.bhargavi\\Desktop\\owtest15.txt","C:\\Users\\g.bhargavi\\Desktop\\owtest15.csv")
+
+
+
 
 
 
